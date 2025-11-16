a072587f5a8bde1b5ce37c8566b4dbe0092760f3
Oghosa1 <guerrilladev@gmail.com>
2025-11-16 21:42:05 +0100

    Initial commit for edge branch

---
+++ lib/controllers/exports.dart
@@ -0,0 +1,8 @@
+export 'package:jobhub_mobile/controllers/bookmark_provider.dart';
+export 'package:jobhub_mobile/controllers/image_provider.dart';
+export 'package:jobhub_mobile/controllers/jobs_provider.dart';
+export 'package:jobhub_mobile/controllers/login_provider.dart';
+export 'package:jobhub_mobile/controllers/onboarding_provider.dart';
+export 'package:jobhub_mobile/controllers/profile_provider.dart';
+export 'package:jobhub_mobile/controllers/signup_provider.dart';
+export 'package:jobhub_mobile/controllers/zoom_provider.dart';
---
+++ lib/views/common/exports.dart
@@ -0,0 +1,3 @@
+export 'package:jobhub_mobile/constants/app_constants.dart';
+export 'package:jobhub_mobile/views/common/app_style.dart';
+export 'package:jobhub_mobile/views/common/reusable_text.dart';
